7e292b0b0613a70b8916687db71029817b8e4f81
proregular <jumoney1012@gmail.com>
2024-07-16 22:24:03 +0900

    첫번째 커밋(테스트)

---
+++ sgLogisticsSystem/src/test/java/com/sg/ls/SgLogisticsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.ls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgLogisticsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
