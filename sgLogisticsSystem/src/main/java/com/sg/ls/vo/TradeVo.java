6f40a6fe7aa65cbdcbe34e2415a18657e961394f
proregular <jumoney1012@gmail.com>
2024-08-18 18:26:21 +0900

    거래처 vo dto 생

---
+++ sgLogisticsSystem/src/main/java/com/sg/ls/vo/TradeVo.java
@@ -0,0 +1,40 @@
+package com.sg.ls.vo;
+
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class TradeVo {
+	private final String trCd;
+	private final String trNm;
+	private final String ceoNm;
+	private final String addr;
+	private final String upte;
+	private final String trNum;
+	private final String tel;
+	private final String fax;
+	private final String jongmok;
+	
+	@Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final TradeVo tradeVo = (TradeVo) o;
+        return trCd == tradeVo.trCd &&
+        	trNm == tradeVo.trNm &&
+        	ceoNm == tradeVo.ceoNm &&
+            addr == tradeVo.addr &&
+            upte == tradeVo.upte &&
+            trNum == tradeVo.trNum &&
+            tel == tradeVo.tel &&
+            fax == tradeVo.fax &&
+            jongmok == tradeVo.jongmok;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(trCd, trNm, ceoNm, addr, upte, trNum, tel, fax, jongmok);
+    }
+    
+}
